be6ca5382afc7bb8b76a2802280674982be7765c
JangoBrick <mail@jangobrick.de>
2015-10-28 13:25:58 +0100

    Add SVGImage loading & parsing functions
    This commit adds two public and static functions for loading an SVG
    image:
    - fromString to load from an XML string
    - fromFile to load from an XML file
    
    The loaded data is then parsed into nodes and an instance of SVGImage
    containing these nodes is returned.
    
    TODO:
    - Add support for styles (in style attribute but also as standalone
    attributes)
    - Handle child document fragments

--- src/SVGImage.php
+++ src/SVGImage.php
@@ -64,4 +64,101 @@ class SVGImage {
         return $this->toXMLString();
     }
 
+
+
+
+
+    public static function fromString($string) {
+        return self::parse(simplexml_load_string($string));
+    }
+
+    public static function fromFile($file) {
+        return self::parse(simplexml_load_file($file));
+    }
+
+
+
+
+
+    // Give it a SimpleXML element of type <svg>, it will return an SVGImage.
+    private static function parse($svg) {
+
+        $image = new self($svg['width'], $svg['height']);
+        $doc = $image->getDocument();
+
+        $children = $svg->children();
+        foreach ($children as $child) {
+            $doc->addChild(self::parseNode($child));
+        }
+
+        return $image;
+
+    }
+
+    // Expects a SimpleXML element as the only parameter.
+    // It will parse the node and any possible children and return an instance
+    // of the appropriate class (e.g. SVGRect or SVGGroup).
+    private static function parseNode($node) {
+
+        $type = $node->getName();
+
+        if ($type === 'g') {
+
+            $element = new SVGGroup();
+
+            $children = $node->children();
+            foreach ($children as $child) {
+                $element->addChild(self::parseNode($child));
+            }
+
+        } else if ($type === 'rect') {
+
+            $w = isset($node['width']) ? $node['width'] : 0;
+            $h = isset($node['height']) ? $node['height'] : 0;
+            $x = isset($node['x']) ? $node['x'] : 0;
+            $y = isset($node['y']) ? $node['y'] : 0;
+
+            $element = new SVGRect($x, $y, $w, $h);
+
+        } else if ($type === 'circle') {
+
+            $cx = isset($node['cx']) ? $node['cx'] : 0;
+            $cy = isset($node['cy']) ? $node['cy'] : 0;
+            $r = isset($node['r']) ? $node['r'] : 0;
+
+            $element = new SVGCircle($cx, $cy, $r);
+
+        } else if ($type === 'ellipse') {
+
+            $cx = isset($node['cx']) ? $node['cx'] : 0;
+            $cy = isset($node['cy']) ? $node['cy'] : 0;
+            $rx = isset($node['rx']) ? $node['rx'] : 0;
+            $ry = isset($node['ry']) ? $node['ry'] : 0;
+
+            $element = new SVGEllipse($cx, $cy, $rx, $ry);
+
+        } else if ($type === 'line') {
+
+            $x1 = isset($node['x1']) ? $node['x1'] : 0;
+            $y1 = isset($node['y1']) ? $node['y1'] : 0;
+            $x2 = isset($node['x2']) ? $node['x2'] : 0;
+            $y2 = isset($node['y2']) ? $node['y2'] : 0;
+
+            $element = new SVGLine($x1, $y1, $x2, $y2);
+
+        } else if ($type === 'polygon' || $type === 'polyline') {
+
+            $element = $type === 'polygon' ? new SVGPolygon() : new SVGPolyline();
+
+            $points = isset($node['points']) ? preg_split('/[\\s,]+/', $node['points']) : array();
+            for ($i=0, $n=floor(count($points)/2); $i<$n; $i++) {
+                $element->addPoint($points[$i*2], $points[$i*2 + 1]);
+            }
+
+        }
+
+        return $element;
+
+    }
+
 }
