0f1a7838082c6ba8fa05c4ea9eb532df44547152
JangoBrick <mail@jangobrick.de>
2015-10-30 14:52:54 +0100

    Add "state stack" to SVGRenderingHelper
    This commit introduces the concept of a stack of translations and
    rendering options to the rendering helper class (SVGRenderingHelper).
    
    The way this works is the following:
    1. A parent node calls push() to save the current state.
    2. It applies transformations and possibly draws itself.
    3. If there are any, it renders its children. Since the transformations
    are stored on the renderer instance, they still apply.
    4. The parent node then calls pop() to restore the transformations that
    were active before the call to push().
    
    That concept allows modifying drawing parameters for a node and its
    children while not affecting parents or siblings that are drawn
    afterwards, since everything will be restored with pop().

--- src/SVGRenderingHelper.php
+++ src/SVGRenderingHelper.php
@@ -4,13 +4,22 @@ class SVGRenderingHelper {
 
     private $image, $imageWidth, $imageHeight;
     private $strokeWidth;
+    private $state, $stateStack;
 
 
 
     public function __construct($image, $imageWidth, $imageHeight) {
+
         $this->image = $image;
         $this->imageWidth = $imageWidth;
         $this->imageHeight = $imageHeight;
+
+        $this->state = array(
+            'x' => 0,
+            'y' => 0,
+        );
+        $this->stateStack = array(&$this->state);
+
     }
 
 
@@ -38,11 +47,40 @@ class SVGRenderingHelper {
 
 
 
+    public function push() {
+        // create copy
+        $this->state = $this->state;
+        $this->stateStack[] = $this->state;
+    }
+
+    public function pop() {
+        if (count($this->stateStack) === 1)
+            throw new RuntimeException("No more states to pop");
+        $this->state = array_pop($this->stateStack);
+    }
+
+
+
+
+
+    public function translate($x, $y) {
+        $this->state['x'] += $x;
+        $this->state['y'] += $y;
+    }
+
+
+
+
+
     public function drawRect($x, $y, $width, $height, $color) {
+        $x += $this->state['x'];
+        $y += $this->state['y'];
         imagerectangle($this->image, $x, $y, $x + $width - 1, $y + $height - 1, $color);
     }
 
     public function fillRect($x, $y, $width, $height, $color) {
+        $x += $this->state['x'];
+        $y += $this->state['y'];
         imagefilledrectangle($this->image, $x, $y, $x + $width - 1, $y + $height - 1, $color);
     }
 
@@ -51,11 +89,15 @@ class SVGRenderingHelper {
 
 
     public function drawEllipse($cx, $cy, $rx, $ry, $color) {
+        $cx += $this->state['x'];
+        $cy += $this->state['y'];
         // imageellipse ignores imagesetthickness; draw arc instead
         imagearc($this->image, $cx, $cy, $rx * 2, $ry * 2, 0, 360, $color);
     }
 
     public function fillEllipse($cx, $cy, $rx, $ry, $color) {
+        $cx += $this->state['x'];
+        $cy += $this->state['y'];
         imagefilledellipse($this->image, $cx, $cy, $rx * 2, $ry * 2, $color);
     }
 
@@ -64,6 +106,10 @@ class SVGRenderingHelper {
 
 
     public function drawLine($x1, $y1, $x2, $y2, $color) {
+        $x1 += $this->state['x'];
+        $y1 += $this->state['y'];
+        $x2 += $this->state['x'];
+        $y2 += $this->state['y'];
         imageline($this->image, $x1, $y1, $x2, $y2, $color);
     }
 
@@ -72,10 +118,16 @@ class SVGRenderingHelper {
 
 
     public function drawPolygon($points, $numpoints, $color) {
+        for ($i=0, $n=count($points); $i<$n; $i++) {
+            $points[$i] += $this->state[$i % 2 === 0 ? 'x' : 'y'];
+        }
         imagepolygon($this->image, $points, $numpoints, $color);
     }
 
     public function fillPolygon($points, $numpoints, $color) {
+        for ($i=0, $n=count($points); $i<$n; $i++) {
+            $points[$i] += $this->state[$i % 2 === 0 ? 'x' : 'y'];
+        }
         imagefilledpolygon($this->image, $points, $numpoints, $color);
     }
 
@@ -84,10 +136,10 @@ class SVGRenderingHelper {
     public function drawPolyline($points, $numpoints, $color) {
 
         for ($i=1; $i<$numpoints; $i++) {
-            $x1 = $points[($i-1) * 2];
-            $y1 = $points[($i-1) * 2 + 1];
-            $x2 = $points[$i * 2];
-            $y2 = $points[$i * 2 + 1];
+            $x1 = $this->state['x'] + $points[($i-1) * 2];
+            $y1 = $this->state['y'] + $points[($i-1) * 2 + 1];
+            $x2 = $this->state['x'] + $points[$i * 2];
+            $y2 = $this->state['y'] + $points[$i * 2 + 1];
             imageline($this->image, $x1, $y1, $x2, $y2, $color);
         }
 
