7ff3521809e4016154121bf64782ec1405e85f0a
JangoBrick <mail@jangobrick.de>
2015-10-30 15:53:03 +0100

    Add opacity scaling to SVGRenderingHelper
    To add to the previous concept of rendering states, the rendering helper
    receives support for applying an opacity factor to every subsequent
    drawing operation. For example, if you call scaleOpacity(0.5), every
    drawn shape will only have half the transparency.

--- src/SVGRenderingHelper.php
+++ src/SVGRenderingHelper.php
@@ -17,6 +17,7 @@ class SVGRenderingHelper {
         $this->state = array(
             'x' => 0,
             'y' => 0,
+            'opacity' => 1
         );
         $this->stateStack = array(&$this->state);
 
@@ -72,15 +73,39 @@ class SVGRenderingHelper {
 
 
 
+    public function scaleOpacity($value) {
+        $this->state['opacity'] *= $value;
+        if ($this->state['opacity'] < 0)
+            $this->state['opacity'] = 0;
+        elseif ($this->state['opacity'] > 1)
+            $this->state['opacity'] = 1;
+    }
+
+    // multiplies the alpha channel of the given color with the given factor
+    // example: color has 0.5 opacity, factor is 0.2 => result is 0.1
+    // the color is a "normal" GD color integer, e.g. 0x7F000000 for fully transparent black
+    private static function _multiplyColorAlpha($color, $alphaFactor) {
+        $rgb = $color & 0x00FFFFFF;
+        $a = ($color & 0xFF000000) >> 24;
+        $a = 0x7F - 0x7F*$alphaFactor + $a*$alphaFactor;
+        return $rgb | ($a << 24);
+    }
+
+
+
+
+
     public function drawRect($x, $y, $width, $height, $color) {
         $x += $this->state['x'];
         $y += $this->state['y'];
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imagerectangle($this->image, $x, $y, $x + $width - 1, $y + $height - 1, $color);
     }
 
     public function fillRect($x, $y, $width, $height, $color) {
         $x += $this->state['x'];
         $y += $this->state['y'];
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imagefilledrectangle($this->image, $x, $y, $x + $width - 1, $y + $height - 1, $color);
     }
 
@@ -91,6 +116,7 @@ class SVGRenderingHelper {
     public function drawEllipse($cx, $cy, $rx, $ry, $color) {
         $cx += $this->state['x'];
         $cy += $this->state['y'];
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         // imageellipse ignores imagesetthickness; draw arc instead
         imagearc($this->image, $cx, $cy, $rx * 2, $ry * 2, 0, 360, $color);
     }
@@ -98,6 +124,7 @@ class SVGRenderingHelper {
     public function fillEllipse($cx, $cy, $rx, $ry, $color) {
         $cx += $this->state['x'];
         $cy += $this->state['y'];
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imagefilledellipse($this->image, $cx, $cy, $rx * 2, $ry * 2, $color);
     }
 
@@ -110,6 +137,7 @@ class SVGRenderingHelper {
         $y1 += $this->state['y'];
         $x2 += $this->state['x'];
         $y2 += $this->state['y'];
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imageline($this->image, $x1, $y1, $x2, $y2, $color);
     }
 
@@ -121,6 +149,7 @@ class SVGRenderingHelper {
         for ($i=0, $n=count($points); $i<$n; $i++) {
             $points[$i] += $this->state[$i % 2 === 0 ? 'x' : 'y'];
         }
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imagepolygon($this->image, $points, $numpoints, $color);
     }
 
@@ -128,12 +157,15 @@ class SVGRenderingHelper {
         for ($i=0, $n=count($points); $i<$n; $i++) {
             $points[$i] += $this->state[$i % 2 === 0 ? 'x' : 'y'];
         }
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
         imagefilledpolygon($this->image, $points, $numpoints, $color);
     }
 
 
 
     public function drawPolyline($points, $numpoints, $color) {
+
+        $color = self::_multiplyColorAlpha($color, $this->state['opacity']);
 
         for ($i=1; $i<$numpoints; $i++) {
             $x1 = $this->state['x'] + $points[($i-1) * 2];
