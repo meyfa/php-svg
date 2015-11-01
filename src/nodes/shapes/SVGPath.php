760ea4f8a30256a9e222fd1bbacaccf8b7386601
JangoBrick <mail@jangobrick.de>
2015-11-01 14:49:41 +0100

    SVGPath: Add draw support for MoveTo, LineTo, Close

--- src/nodes/shapes/SVGPath.php
+++ src/nodes/shapes/SVGPath.php
@@ -40,6 +40,91 @@ class SVGPath extends SVGNode {
 
     public function draw(SVGRenderingHelper $rh, $scaleX, $scaleY, $offsetX = 0, $offsetY = 0) {
 
+        $rh->push();
+
+        $opacity = $this->getStyle('opacity');
+        if (isset($opacity) && is_numeric($opacity)) {
+            $opacity = floatval($opacity);
+            $rh->scaleOpacity($opacity);
+        }
+
+        // original (document fragment) width for unit parsing
+        $ow = $rh->getWidth() / $scaleX;
+
+        $stroke = $this->getComputedStyle('stroke');
+        if (isset($stroke) && $stroke !== 'none') {
+
+            $strokeColor = SVG::parseColor($stroke, true);
+            $rh->setStrokeWidth(SVG::convertUnit($this->getComputedStyle('stroke-width'), $ow) * $scaleX);
+
+            $x = 0;
+            $y = 0;
+            $startX = null;
+            $startY = null;
+
+            $matches = array();
+            preg_match_all('/[MLCAZ][^MLCAZ]*/i', $this->d, $matches, PREG_SET_ORDER);
+
+            foreach ($matches as $match) {
+
+                $match = trim($match[0]);
+                $command = substr($match, 0, 1);
+
+                $args = preg_split('/[\s,]+/', substr($match, 1));
+
+                if ($command === 'M') {
+                    // moveto absolute
+                    $x = floatval($args[0]);
+                    $y = floatval($args[1]);
+                    if ($startX === null) {
+                        $startX = $x;
+                        $startY = $y;
+                    }
+                } else if ($command === 'm') {
+                    // moveto relative
+                    $x += floatval($args[0]);
+                    $y += floatval($args[1]);
+                } else if ($command === 'L') {
+                    // lineto absolute
+                    $nx = floatval($args[0]);
+                    $ny = floatval($args[1]);
+                    $rh->drawLine(
+                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
+                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
+                        $strokeColor
+                    );
+                    $x = $nx;
+                    $y = $ny;
+                } else if ($command === 'l') {
+                    // lineto relative
+                    $nx = $x + floatval($args[0]);
+                    $ny = $y + floatval($args[1]);
+                    $rh->drawLine(
+                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
+                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
+                        $strokeColor
+                    );
+                    $x = $nx;
+                    $y = $ny;
+                } else if ($command === 'Z' || $command === 'z') {
+                    // end
+                    $nx = $startX !== null ? $startX : 0;
+                    $ny = $startY !== null ? $startY : 0;
+                    $rh->drawLine(
+                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
+                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
+                        $strokeColor
+                    );
+                    $x = $nx;
+                    $y = $ny;
+                }
+
+            }
+
+        }
+
+        $rh->pop();
+
     }
 
 }
