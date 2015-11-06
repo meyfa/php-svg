c66d685d014a5f8823a869bb287852a56c897984
JangoBrick <mail@jangobrick.de>
2015-11-06 18:37:12 +0100

    SVGPath: Add fill support
    This commit adds support for filling SVGPath instances when rasterizing.
    That is achieved by converting every subpath into a polygon, then
    drawing and filling those polygons.

--- src/nodes/shapes/SVGPath.php
+++ src/nodes/shapes/SVGPath.php
@@ -51,115 +51,134 @@ class SVGPath extends SVGNode {
         // original (document fragment) width for unit parsing
         $ow = $rh->getWidth() / $scaleX;
 
-        $stroke = $this->getComputedStyle('stroke');
-        if (isset($stroke) && $stroke !== 'none') {
 
-            $strokeColor = SVG::parseColor($stroke, true);
-            $rh->setStrokeWidth(SVG::convertUnit($this->getComputedStyle('stroke-width'), $ow) * $scaleX);
 
-            $x = 0;
-            $y = 0;
-            $startX = null;
-            $startY = null;
+        // start of polygon construction
 
-            $matches = array();
-            preg_match_all('/[MLCQAZ][^MLCQAZ]*/i', $this->d, $matches, PREG_SET_ORDER);
+        $polys = array();
+        $currentPoly = null;
 
-            foreach ($matches as $match) {
+        $x = 0;
+        $y = 0;
+        $startX = null;
+        $startY = null;
 
-                $match = trim($match[0]);
-                $command = substr($match, 0, 1);
+        $matches = array();
+        preg_match_all('/[MLCQAZ][^MLCQAZ]*/i', $this->d, $matches, PREG_SET_ORDER);
 
-                $args = preg_split('/[\s,]+/', trim(substr($match, 1)));
+        foreach ($matches as $match) {
 
-                if ($command === 'M') {
-                    // moveto absolute
-                    $x = floatval($args[0]);
-                    $y = floatval($args[1]);
-                    $startX = $x;
-                    $startY = $y;
-                } else if ($command === 'm') {
-                    // moveto relative
-                    $x += floatval($args[0]);
-                    $y += floatval($args[1]);
-                    $startX = $x;
-                    $startY = $y;
-                } else if ($command === 'L') {
-                    // lineto absolute
-                    $nx = floatval($args[0]);
-                    $ny = floatval($args[1]);
-                    $rh->drawLine(
-                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
-                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
-                        $strokeColor
-                    );
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'l') {
-                    // lineto relative
-                    $nx = $x + floatval($args[0]);
-                    $ny = $y + floatval($args[1]);
-                    $rh->drawLine(
-                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
-                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
-                        $strokeColor
-                    );
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'Z' || $command === 'z') {
-                    // end
-                    $nx = $startX !== null ? $startX : 0;
-                    $ny = $startY !== null ? $startY : 0;
-                    $rh->drawLine(
-                        ($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY, // x1, y1
-                        ($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY, // x2, y2
-                        $strokeColor
-                    );
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'C') {
-                    $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
-                    $p1 = array(($offsetX + floatval($args[0])) * $scaleX, ($offsetY + floatval($args[1])) * $scaleY);
-                    $p2 = array(($offsetX + floatval($args[2])) * $scaleX, ($offsetY + floatval($args[3])) * $scaleY);
-                    $nx = floatval($args[4]);
-                    $ny = floatval($args[5]);
-                    $p3 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
-                    $rh->drawCubicBezier($p0, $p1, $p2, $p3, $strokeColor);
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'c') {
-                    $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
-                    $p1 = array(($offsetX + $x + floatval($args[0])) * $scaleX, ($offsetY + $y + floatval($args[1])) * $scaleY);
-                    $p2 = array(($offsetX + $x + floatval($args[2])) * $scaleX, ($offsetY + $y + floatval($args[3])) * $scaleY);
-                    $nx = $x + floatval($args[4]);
-                    $ny = $y + floatval($args[5]);
-                    $p3 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
-                    $rh->drawCubicBezier($p0, $p1, $p2, $p3, $strokeColor);
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'Q') {
-                    $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
-                    $p1 = array(($offsetX + floatval($args[0])) * $scaleX, ($offsetY + floatval($args[1])) * $scaleY);
-                    $nx = floatval($args[2]);
-                    $ny = floatval($args[3]);
-                    $p2 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
-                    $rh->drawQuadraticBezier($p0, $p1, $p2, $strokeColor);
-                    $x = $nx;
-                    $y = $ny;
-                } else if ($command === 'q') {
-                    $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
-                    $p1 = array(($offsetX + $x + floatval($args[0])) * $scaleX, ($offsetY + $y + floatval($args[1])) * $scaleY);
-                    $nx = $x + floatval($args[2]);
-                    $ny = $y + floatval($args[3]);
-                    $p2 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
-                    $rh->drawQuadraticBezier($p0, $p1, $p2, $strokeColor);
-                    $x = $nx;
-                    $y = $ny;
+            $match = trim($match[0]);
+            $command = substr($match, 0, 1);
+
+            $args = preg_split('/[\s,]+/', trim(substr($match, 1)));
+
+            if ($command === 'M') {
+                // moveto absolute
+                $x = floatval($args[0]);
+                $y = floatval($args[1]);
+                $startX = $x;
+                $startY = $y;
+                if (!empty($currentPoly)) {
+                    $polys[] = $currentPoly;
                 }
-
+                $currentPoly = array();
+            } else if ($command === 'm') {
+                // moveto relative
+                $x += floatval($args[0]);
+                $y += floatval($args[1]);
+                $startX = $x;
+                $startY = $y;
+                if (!empty($currentPoly)) {
+                    $polys[] = $currentPoly;
+                }
+                $currentPoly = array();
+            } else if ($command === 'L') {
+                // lineto absolute
+                $x = floatval($args[0]);
+                $y = floatval($args[1]);
+                $currentPoly[] = ($offsetX + $x) * $scaleX;
+                $currentPoly[] = ($offsetY + $y) * $scaleY;
+            } else if ($command === 'l') {
+                // lineto relative
+                $x += floatval($args[0]);
+                $y += floatval($args[1]);
+                $currentPoly[] = ($offsetX + $x) * $scaleX;
+                $currentPoly[] = ($offsetY + $y) * $scaleY;
+            } else if ($command === 'Z' || $command === 'z') {
+                // end
+                $x = $startX !== null ? $startX : 0;
+                $y = $startY !== null ? $startY : 0;
+                $currentPoly[] = ($offsetX + $x) * $scaleX;
+                $currentPoly[] = ($offsetY + $y) * $scaleY;
+            } else if ($command === 'C') {
+                $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
+                $p1 = array(($offsetX + floatval($args[0])) * $scaleX, ($offsetY + floatval($args[1])) * $scaleY);
+                $p2 = array(($offsetX + floatval($args[2])) * $scaleX, ($offsetY + floatval($args[3])) * $scaleY);
+                $nx = floatval($args[4]);
+                $ny = floatval($args[5]);
+                $p3 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
+                $currentPoly = array_merge($currentPoly, SVGRenderingHelper::approximateCubicBezier($p0, $p1, $p2, $p3));
+                $x = $nx;
+                $y = $ny;
+            } else if ($command === 'c') {
+                $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
+                $p1 = array(($offsetX + $x + floatval($args[0])) * $scaleX, ($offsetY + $y + floatval($args[1])) * $scaleY);
+                $p2 = array(($offsetX + $x + floatval($args[2])) * $scaleX, ($offsetY + $y + floatval($args[3])) * $scaleY);
+                $nx = $x + floatval($args[4]);
+                $ny = $y + floatval($args[5]);
+                $p3 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
+                $currentPoly = array_merge($currentPoly, SVGRenderingHelper::approximateCubicBezier($p0, $p1, $p2, $p3));
+                $x = $nx;
+                $y = $ny;
+            } else if ($command === 'Q') {
+                $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
+                $p1 = array(($offsetX + floatval($args[0])) * $scaleX, ($offsetY + floatval($args[1])) * $scaleY);
+                $nx = floatval($args[2]);
+                $ny = floatval($args[3]);
+                $p2 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
+                $currentPoly = array_merge($currentPoly, SVGRenderingHelper::approximateQuadraticBezier($p0, $p1, $p2));
+                $x = $nx;
+                $y = $ny;
+            } else if ($command === 'q') {
+                $p0 = array(($offsetX + $x) * $scaleX, ($offsetY + $y) * $scaleY);
+                $p1 = array(($offsetX + $x + floatval($args[0])) * $scaleX, ($offsetY + $y + floatval($args[1])) * $scaleY);
+                $nx = $x + floatval($args[2]);
+                $ny = $y + floatval($args[3]);
+                $p2 = array(($offsetX + $nx) * $scaleX, ($offsetY + $ny) * $scaleY);
+                $currentPoly = array_merge($currentPoly, SVGRenderingHelper::approximateQuadraticBezier($p0, $p1, $p2));
+                $x = $nx;
+                $y = $ny;
             }
 
         }
+
+        if (!empty($currentPoly)) {
+            $polys[] = $currentPoly;
+        }
+
+
+
+        // outline
+
+        $fill = $this->getComputedStyle('fill');
+        if (isset($fill) && $fill !== 'none') {
+            $fillColor = SVG::parseColor($fill, true);
+            foreach ($polys as $poly) {
+                $rh->fillPolygon($poly, count($poly) / 2, $fillColor);
+            }
+        }
+
+        $stroke = $this->getComputedStyle('stroke');
+        if (isset($stroke) && $stroke !== 'none') {
+            $strokeColor = SVG::parseColor($stroke, true);
+            $rh->setStrokeWidth(SVG::convertUnit($this->getComputedStyle('stroke-width'), $ow) * $scaleX);
+            foreach ($polys as $poly) {
+                $rh->drawPolyline($poly, count($poly) / 2, $strokeColor);
+            }
+        }
+
+
 
         $rh->pop();
 
