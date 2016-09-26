ca8c0a2bfa73881a466088aa27e328780ab258b9
JangoBrick <mail@jangobrick.de>
2016-09-26 12:18:54 +0200

    Make polygon/-line subclasses of SVGPolygonalShape
    SVGPolygon and SVGPolyline shared almost the exact same code, with the
    exception of outline drawing. A new common parent class,
    SVGPolygonalShape, aims to fix that by providing an abstract method for
    that purpose, and handling the rest (point manipulation,
    stringification, ...) completely on its own.

--- src/Nodes/Shapes/SVGPolygon.php
+++ src/Nodes/Shapes/SVGPolygon.php
@@ -2,115 +2,16 @@
 
 namespace JangoBrick\SVG\Nodes\Shapes;
 
-use JangoBrick\SVG\Nodes\SVGNode;
-use JangoBrick\SVG\SVG;
 use JangoBrick\SVG\SVGRenderingHelper;
 
-class SVGPolygon extends SVGNode
+class SVGPolygon extends SVGPolygonalShape
 {
-    private $points;
-
     public function __construct($points = array())
     {
-        parent::__construct();
-
-        $this->points = $points;
+        parent::__construct('polygon', $points);
     }
 
-    public function addPoint($a, $b = null)
-    {
-        if (!is_array($a)) {
-            $a = array($a, $b);
-        }
-
-        $this->points[] = $a;
-        return $this;
-    }
-
-    public function removePoint($index)
-    {
-        array_splice($this->points, $index, 1);
-        return $this;
-    }
-
-    public function countPoints()
-    {
-        return count($this->points);
-    }
-
-    public function getPoints()
-    {
-        return $this->points;
-    }
-
-    public function getPoint($index)
-    {
-        return $this->points[$index];
-    }
-
-    public function setPoint($index, $point)
-    {
-        $this->points[$index] = $point;
-        return $this;
-    }
-
-    public function toXMLString()
-    {
-        $s  = '<polygon';
-
-        $s .= ' points="';
-        for ($i = 0, $n = count($this->points); $i < $n; ++$i) {
-            $point = $this->points[$i];
-            if ($i > 0) {
-                $s .= ' ';
-            }
-            $s .= $point[0].','.$point[1];
-        }
-        $s .= '"';
-
-        $this->addStylesToXMLString($s);
-        $this->addAttributesToXMLString($s);
-
-        $s .= ' />';
-
-        return $s;
-    }
-
-    public function draw(SVGRenderingHelper $rh, $scaleX, $scaleY, $offsetX = 0, $offsetY = 0)
-    {
-        $rh->push();
-
-        $opacity = $this->getStyle('opacity');
-        if (isset($opacity) && is_numeric($opacity)) {
-            $opacity = floatval($opacity);
-            $rh->scaleOpacity($opacity);
-        }
-
-        // original (document fragment) width for unit parsing
-        $ow = $rh->getWidth() / $scaleX;
-
-        $p  = array();
-        $np = count($this->points);
-
-        for ($i = 0; $i < $np; ++$i) {
-            $point = $this->points[$i];
-            $p[]   = ($offsetX + $point[0]) * $scaleX;
-            $p[]   = ($offsetY + $point[1]) * $scaleY;
-        }
-
-        $fill = $this->getComputedStyle('fill');
-        if (isset($fill) && $fill !== 'none') {
-            $fillColor = SVG::parseColor($fill, true);
-            $rh->fillPolygon($p, $np, $fillColor);
-        }
-
-        $stroke = $this->getComputedStyle('stroke');
-        if (isset($stroke) && $stroke !== 'none') {
-            $strokeColor = SVG::parseColor($stroke, true);
-            $rh->setStrokeWidth(SVG::convertUnit($this->getComputedStyle('stroke-width'), $ow) * $scaleX);
-            $rh->drawPolygon($p, $np, $strokeColor);
-        }
-
-        $rh->pop();
+    protected function drawOutline(SVGRenderingHelper $rh, $points, $numPoints, $strokeColor) {
+        $rh->drawPolygon($points, $numPoints, $strokeColor);
     }
 }
