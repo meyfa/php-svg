3314e2598a38649d36920f24ebc81ce545eead31
JangoBrick <mail@jangobrick.de>
2016-10-08 19:20:50 +0200

    Add SVGRenderer class
    This class serves as the abstract base class for the specific renderer
    instances to be implemented.
    It holds the 'render' method that deals with preparing all the visual
    attributes before it calls the implementation with the rendering job's
    details.
    
    Subclasses only have to implement 3 abstract methods:
    * prepareRenderParams, which takes an array of options and converts them
    into a new array of parameters to be passed into the following two
    methods. Example tasks for this method include coordinate scaling or
    conversion between a radius and width/height.
    * renderFill, which uses the parameters to draw the shape's fill.
    * renderStroke, which does the same for the stroke.

---
+++ src/Rasterization/Renderers/SVGRenderer.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace JangoBrick\SVG\Rasterization\Renderers;
+
+use JangoBrick\SVG\SVG;
+use JangoBrick\SVG\Nodes\SVGNode;
+use JangoBrick\SVG\Rasterization\SVGRasterizer;
+
+abstract class SVGRenderer
+{
+    public function render(SVGRasterizer $rasterizer, array $options, SVGNode $context)
+    {
+        $params = $this->prepareRenderParams($rasterizer, $options);
+
+        $image = $rasterizer->getImage();
+
+        $fill = $context->getComputedStyle('fill');
+        if (isset($fill) && $fill !== 'none') {
+            $fill = self::prepareColor($fill, $context);
+
+            $this->renderFill($image, $params, $fill);
+        }
+
+        $stroke = $context->getComputedStyle('stroke');
+        if (isset($stroke) && $stroke !== 'none') {
+            $stroke      = self::prepareColor($stroke, $context);
+            $strokeWidth = $context->getComputedStyle('stroke-width');
+            $strokeWidth = self::prepareLength($strokeWidth, $rasterizer);
+
+            $this->renderStroke($image, $params, $stroke, $strokeWidth);
+        }
+    }
+
+
+
+    private static function prepareColor($color, SVGNode $context)
+    {
+        $color = SVG::parseColor($color, true);
+
+        $alphaFactor = self::calculateTotalOpacity($context);
+
+        $rgb = $color & 0x00FFFFFF;
+        $a   = ($color & 0xFF000000) >> 24;
+        $a   = 0x7F - 0x7F * $alphaFactor + $a * $alphaFactor;
+
+        return $rgb | ($a << 24);
+    }
+
+    private static function getNodeOpacity(SVGNode $node)
+    {
+        $opacity = $node->getStyle('opacity');
+
+        if (is_numeric($opacity)) {
+            return floatval($opacity);
+        } elseif ($opacity === 'inherit') {
+            $parent = $node->getParent();
+            if (isset($parent)) {
+                return self::getNodeOpacity($parent);
+            }
+        }
+
+        return 1;
+    }
+
+    private static function calculateTotalOpacity(SVGNode $node)
+    {
+        $opacity = self::getNodeOpacity($node);
+
+        $parent  = $node->getParent();
+        if (isset($parent)) {
+            return $opacity * self::calculateTotalOpacity($parent);
+        }
+
+        return $opacity;
+    }
+
+
+
+    private static function prepareLength($len, SVGRasterizer $rasterizer)
+    {
+        $docWidth = $rasterizer->getDocumentWidth();
+        $scaleX   = $rasterizer->getScaleX();
+
+        return SVG::convertUnit($len, $docWidth) * $scaleX;
+    }
+
+
+
+    abstract protected function prepareRenderParams(SVGRasterizer $rasterizer, array $options);
+
+    abstract protected function renderFill($image, array $params, $color);
+
+    abstract protected function renderStroke($image, array $params, $color, $strokeWidth);
+}
