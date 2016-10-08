1ee2e2ea2087271b108c420a788558829472064e
JangoBrick <mail@jangobrick.de>
2016-10-08 19:10:45 +0200

    Add SVGRasterizer class
    The SVGRasterizer class will handle all of the rasterization process, by
    delegating tasks to specific renderer instances.
    Through this, it will be possible to eliminate SVGRenderingHelper, which
    has become quite impractical to use, and greatly reduce rendering code
    in SVGNode subclasses.

---
+++ src/Rasterization/SVGRasterizer.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace JangoBrick\SVG\Rasterization;
+
+use JangoBrick\SVG\Nodes\SVGNode;
+
+class SVGRasterizer
+{
+    private static $renderers;
+
+    private $docWidth, $docHeight;
+    private $width, $height;
+    private $scaleX, $scaleY;
+    private $outImage;
+
+    public function __construct($docWidth, $docHeight, $width, $height)
+    {
+        $this->docWidth  = $docWidth;
+        $this->docHeight = $docHeight;
+
+        $this->width  = $width;
+        $this->height = $height;
+
+        $this->scaleX = $width / $docWidth;
+        $this->scaleY = $height / $docHeight;
+
+        $this->outImage = self::createImage($width, $height);
+    }
+
+    private static function createImage($width, $height)
+    {
+        $img = imagecreatetruecolor($width, $height);
+
+        imagealphablending($img, true);
+        imagesavealpha($img, true);
+
+        imagefill($img, 0, 0, 0x7F000000);
+
+        return $img;
+    }
+
+
+
+    private static function getRenderer($id)
+    {
+        if (!isset(self::$renderers[$id])) {
+            throw new \InvalidArgumentException("no such renderer: ".$id);
+        }
+        return self::$renderers[$id];
+    }
+
+    public function render($rendererId, array $params, SVGNode $context)
+    {
+        return (self::getRenderer($rendererId))
+            ->render($this, $params, $context);
+    }
+
+
+
+    public function getDocumentWidth()
+    {
+        return $this->docWidth;
+    }
+
+    public function getDocumentHeight()
+    {
+        return $this->docHeight;
+    }
+
+
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+
+
+    public function getScaleX()
+    {
+        return $this->scaleX;
+    }
+
+    public function getScaleY()
+    {
+        return $this->scaleY;
+    }
+
+
+
+    public function getImage()
+    {
+        return $this->outImage;
+    }
+}
