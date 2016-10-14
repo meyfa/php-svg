52e96bee4647c8a4efd8c1596b3e401ee1f872de
JangoBrick <mail@jangobrick.de>
2016-10-14 23:13:28 +0200

    Fix Travis/PHPUnit errors

--- tests/SVGImageTest.php
+++ tests/SVGImageTest.php
@@ -1,18 +1,22 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-
 use JangoBrick\SVG\SVGImage;
 use JangoBrick\SVG\Nodes\Structures\SVGDocumentFragment;
 
-class SVGImageTest extends TestCase
+class SVGImageTest extends PHPUnit_Framework_TestCase
 {
-    private static $xml = '<?xml version="1.0" encoding="utf-8"?>'.
-        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"></svg>';
+    private $xml;
+
+    public function setUp()
+    {
+        $this->xml  = '<?xml version="1.0" encoding="utf-8"?>';
+        $this->xml .= '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10">';
+        $this->xml .= '</svg>';
+    }
 
     public function testGetDocument()
     {
-        $image = SVGImage::fromString(self::$xml);
+        $image = SVGImage::fromString($this->xml);
 
         $doc = $image->getDocument();
         $this->assertInstanceOf(SVGDocumentFragment::class, $doc);
@@ -20,7 +24,7 @@ class SVGImageTest extends TestCase
 
     public function testToRasterImage()
     {
-        $image = SVGImage::fromString(self::$xml);
+        $image = SVGImage::fromString($this->xml);
 
         $rasterImage = $image->toRasterImage(100, 100);
         $this->assertTrue(is_resource($rasterImage));
@@ -32,8 +36,8 @@ class SVGImageTest extends TestCase
 
     public function testToXMLString()
     {
-        $image = SVGImage::fromString(self::$xml);
+        $image = SVGImage::fromString($this->xml);
 
-        $this->assertSame(self::$xml, $image->toXMLString());
+        $this->assertSame($this->xml, $image->toXMLString());
     }
 }
