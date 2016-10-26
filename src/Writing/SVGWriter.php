25749c818a79b9f1ace4985af4b8ceceadd7cdee
JangoBrick <mail@jangobrick.de>
2016-10-26 17:46:18 +0200

    (doc) Add doc comments to SVGWriter

--- src/Writing/SVGWriter.php
+++ src/Writing/SVGWriter.php
@@ -5,6 +5,10 @@ namespace JangoBrick\SVG\Writing;
 use JangoBrick\SVG\Nodes\SVGNode;
 use JangoBrick\SVG\Nodes\SVGNodeContainer;
 
+/**
+ * This class is used for composing ("writing") XML strings from nodes.
+ * Every instance corresponds to one output string.
+ */
 class SVGWriter
 {
     private $outString;
@@ -14,11 +18,28 @@ class SVGWriter
         $this->outString = '<?xml version="1.0" encoding="utf-8"?>';
     }
 
+    /**
+     * @return string The XML output string up until the point currenly written.
+     */
     public function getString()
     {
         return $this->outString;
     }
 
+    /**
+     * Converts the given node into its XML representation and appends that to
+     * this writer's output string.
+     *
+     * The generated string contains the attributes defined via
+     * SVGNode::getSerializableAttributes() and the styles defined via
+     * SVGNode::getSerializableStyles().
+     * Container nodes (<g></g>) and self-closing tags (<rect />) are
+     * distinguished correctly.
+     *
+     * @param SVGNode $node The node to write.
+     *
+     * @return void
+     */
     public function writeNode(SVGNode $node)
     {
         $this->outString .= '<'.$node->getName();
@@ -38,6 +59,14 @@ class SVGWriter
         $this->outString .= '</'.$node->getName().'>';
     }
 
+    /**
+     * Converts the given styles into a CSS string, then appends a 'style'
+     * attribute with the value set to that string to this writer's output.
+     *
+     * @param string[] $styles An associative array of styles for the attribute.
+     *
+     * @return void
+     */
     private function appendStyles(array $styles)
     {
         if (empty($styles)) {
@@ -57,6 +86,14 @@ class SVGWriter
         $this->appendAttribute('style', $string);
     }
 
+    /**
+     * Appends all attributes defined in the given associative array to this
+     * writer's output.
+     *
+     * @param string[] $attrs An associative array of attribute strings.
+     *
+     * @return void
+     */
     private function appendAttributes(array $attrs)
     {
         foreach ($attrs as $key => $value) {
@@ -64,6 +101,15 @@ class SVGWriter
         }
     }
 
+    /**
+     * Appends a single attribute given by key and value to this writer's
+     * output.
+     *
+     * @param string $attrName  The attribute name.
+     * @param string $attrValue The attribute value.
+     *
+     * @return void
+     */
     private function appendAttribute($attrName, $attrValue)
     {
         $this->outString .= ' '.$attrName.'="'.$attrValue.'"';
