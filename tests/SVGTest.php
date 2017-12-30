dc5a79481847598eebb3c7b2e3a5f68de7b08e3f
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-12-30 23:12:38 +0100

    Add tests for SVG::convertUnit

---
+++ tests/SVGTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use SVG\SVG;
+
+class SVGTest extends PHPUnit_Framework_TestCase
+{
+    public function testConvertUnit()
+    {
+        // units
+        $this->assertEquals(16, SVG::convertUnit('12pt', 100));
+        $this->assertEquals(16, SVG::convertUnit('1pc', 100));
+        $this->assertEquals(37.8, SVG::convertUnit('1cm', 100), '', 0.01);
+        $this->assertEquals(37.8, SVG::convertUnit('10mm', 100), '', 0.01);
+        $this->assertEquals(96, SVG::convertUnit('1in', 100));
+        $this->assertEquals(50, SVG::convertUnit('50%', 100));
+        $this->assertEquals(16, SVG::convertUnit('16px', 100));
+
+        // no unit
+        $this->assertEquals(16, SVG::convertUnit('16', 100));
+
+        // number
+        $this->assertEquals(16, SVG::convertUnit(16, 100));
+    }
+}
