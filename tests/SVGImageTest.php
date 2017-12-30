9bb8c4845213e4e5352c69b0595b7ab4148b63d0
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-12-30 18:52:07 +0100

    Add more tests for SVGImage

--- tests/SVGImageTest.php
+++ tests/SVGImageTest.php
@@ -16,18 +16,23 @@ class SVGImageTest extends PHPUnit_Framework_TestCase
 
     public function testGetDocument()
     {
-        $image = SVGImage::fromString($this->xml);
-
+        $image = new SVGImage(10, 10);
         $doc = $image->getDocument();
+
         $docFragClass = '\SVG\Nodes\Structures\SVGDocumentFragment';
         $this->assertInstanceOf($docFragClass, $doc);
+
+        $this->assertTrue($doc->isRoot());
+
+        $this->assertEquals('10', $doc->getWidth());
+        $this->assertEquals('10', $doc->getHeight());
     }
 
     public function testToRasterImage()
     {
-        $image = SVGImage::fromString($this->xml);
-
+        $image = new SVGImage(10, 10);
         $rasterImage = $image->toRasterImage(100, 100);
+
         $this->assertTrue(is_resource($rasterImage));
         $this->assertSame('gd', get_resource_type($rasterImage));
 
@@ -35,10 +40,31 @@ class SVGImageTest extends PHPUnit_Framework_TestCase
         $this->assertSame(100, imagesy($rasterImage));
     }
 
+    /**
+     * @SuppressWarnings("camelCase")
+     */
+    public function test__toString()
+    {
+        $image = new SVGImage(10, 10);
+
+        $this->assertSame($this->xml, (string) $image);
+    }
+
     public function testToXMLString()
     {
-        $image = SVGImage::fromString($this->xml);
+        $image = new SVGImage(10, 10);
 
         $this->assertSame($this->xml, $image->toXMLString());
+    }
+
+    public function testFromString()
+    {
+        $image = SVGImage::fromString($this->xml);
+        $doc = $image->getDocument();
+
+        $this->assertInstanceOf('\SVG\SVGImage', $image);
+
+        $this->assertEquals('10', $doc->getWidth());
+        $this->assertEquals('10', $doc->getHeight());
     }
 }
