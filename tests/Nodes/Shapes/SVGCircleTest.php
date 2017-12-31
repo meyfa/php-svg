28807b66c04bd04113d460e018375e4a7cf53c5a
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-12-31 15:06:12 +0100

    Add tests for circle, ellipse, line, rect classes

---
+++ tests/Nodes/Shapes/SVGCircleTest.php
@@ -0,0 +1,111 @@
+<?php
+
+use SVG\Nodes\Shapes\SVGCircle;
+
+/**
+ * @SuppressWarnings(PHPMD)
+ */
+ class SVGCircleTest extends PHPUnit_Framework_TestCase
+{
+    public function test__construct()
+    {
+        // should not set any attributes by default
+        $obj = new SVGCircle();
+        $this->assertEquals(array(), $obj->getSerializableAttributes());
+
+        // should set attributes when provided
+        $obj = new SVGCircle(37, 42, 100);
+        $this->assertEquals(array(
+            'cx' => '37',
+            'cy' => '42',
+            'r' => '100',
+        ), $obj->getSerializableAttributes());
+    }
+
+    public function testGetCenterX()
+    {
+        $obj = new SVGCircle();
+
+        // should return the attribute
+        $obj->setAttribute('cx', 42);
+        $this->assertSame('42', $obj->getCenterX());
+    }
+
+    public function testSetCenterX()
+    {
+        $obj = new SVGCircle();
+
+        // should update the attribute
+        $obj->setCenterX(42);
+        $this->assertSame('42', $obj->getAttribute('cx'));
+    }
+
+    public function testGetCenterY()
+    {
+        $obj = new SVGCircle();
+
+        // should return the attribute
+        $obj->setAttribute('cy', 42);
+        $this->assertSame('42', $obj->getCenterY());
+    }
+
+    public function testSetCenterY()
+    {
+        $obj = new SVGCircle();
+
+        // should update the attribute
+        $obj->setCenterY(42);
+        $this->assertSame('42', $obj->getAttribute('cy'));
+    }
+
+    public function testGetRadius()
+    {
+        $obj = new SVGCircle();
+
+        // should return the attribute
+        $obj->setAttribute('r', 42);
+        $this->assertSame('42', $obj->getRadius());
+    }
+
+    public function testSetRadius()
+    {
+        $obj = new SVGCircle();
+
+        // should update the attribute
+        $obj->setRadius(42);
+        $this->assertSame('42', $obj->getAttribute('r'));
+    }
+
+    public function testRasterize()
+    {
+        $obj = new SVGCircle(37, 42, 100);
+
+        $rast = $this->getMockBuilder('\SVG\Rasterization\SVGRasterizer')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // should call image renderer with correct options
+        $rast->expects($this->once())->method('render')->with(
+            $this->equalTo('ellipse'),
+            $this->equalTo(array(
+                'cx' => '37',
+                'cy' => '42',
+                'rx' => '100',
+                'ry' => '100',
+            )),
+            $this->identicalTo($obj)
+        );
+        $obj->rasterize($rast);
+
+        // should not rasterize with 'display: none' style
+        $obj->setStyle('display', 'none');
+        $obj->rasterize($rast);
+
+        // should not rasterize with 'visibility: hidden' or 'collapse' style
+        $obj->setStyle('display', null);
+        $obj->setStyle('visibility', 'hidden');
+        $obj->rasterize($rast);
+        $obj->setStyle('visibility', 'collapse');
+        $obj->rasterize($rast);
+    }
+}
---
+++ tests/Nodes/Shapes/SVGLineTest.php
@@ -0,0 +1,130 @@
+<?php
+
+use SVG\Nodes\Shapes\SVGLine;
+
+/**
+ * @SuppressWarnings(PHPMD)
+ */
+ class SVGLineTest extends PHPUnit_Framework_TestCase
+{
+    public function test__construct()
+    {
+        // should not set any attributes by default
+        $obj = new SVGLine();
+        $this->assertEquals(array(), $obj->getSerializableAttributes());
+
+        // should set attributes when provided
+        $obj = new SVGLine(11, 12, 13, 14);
+        $this->assertEquals(array(
+            'x1' => '11',
+            'y1' => '12',
+            'x2' => '13',
+            'y2' => '14'
+        ), $obj->getSerializableAttributes());
+    }
+
+    public function testGetX1()
+    {
+        $obj = new SVGLine();
+
+        // should return the attribute
+        $obj->setAttribute('x1', 42);
+        $this->assertSame('42', $obj->getX1());
+    }
+
+    public function testSetX1()
+    {
+        $obj = new SVGLine();
+
+        // should update the attribute
+        $obj->setX1(42);
+        $this->assertSame('42', $obj->getAttribute('x1'));
+    }
+
+    public function testGetY1()
+    {
+        $obj = new SVGLine();
+
+        // should return the attribute
+        $obj->setAttribute('y1', 42);
+        $this->assertSame('42', $obj->getY1());
+    }
+
+    public function testSetY1()
+    {
+        $obj = new SVGLine();
+
+        // should update the attribute
+        $obj->setY1(42);
+        $this->assertSame('42', $obj->getAttribute('y1'));
+    }
+
+    public function testGetX2()
+    {
+        $obj = new SVGLine();
+
+        // should return the attribute
+        $obj->setAttribute('x2', 42);
+        $this->assertSame('42', $obj->getX2());
+    }
+
+    public function testSetX2()
+    {
+        $obj = new SVGLine();
+
+        // should update the attribute
+        $obj->setX2(42);
+        $this->assertSame('42', $obj->getAttribute('x2'));
+    }
+
+    public function testGetY2()
+    {
+        $obj = new SVGLine();
+
+        // should return the attribute
+        $obj->setAttribute('y2', 42);
+        $this->assertSame('42', $obj->getY2());
+    }
+
+    public function testSetY2()
+    {
+        $obj = new SVGLine();
+
+        // should update the attribute
+        $obj->setY2(42);
+        $this->assertSame('42', $obj->getAttribute('y2'));
+    }
+
+    public function testRasterize()
+    {
+        $obj = new SVGLine(11, 12, 13, 14);
+
+        $rast = $this->getMockBuilder('\SVG\Rasterization\SVGRasterizer')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // should call image renderer with correct options
+        $rast->expects($this->once())->method('render')->with(
+            $this->equalTo('line'),
+            $this->equalTo(array(
+                'x1' => '11',
+                'y1' => '12',
+                'x2' => '13',
+                'y2' => '14',
+            )),
+            $this->identicalTo($obj)
+        );
+        $obj->rasterize($rast);
+
+        // should not rasterize with 'display: none' style
+        $obj->setStyle('display', 'none');
+        $obj->rasterize($rast);
+
+        // should not rasterize with 'visibility: hidden' or 'collapse' style
+        $obj->setStyle('display', null);
+        $obj->setStyle('visibility', 'hidden');
+        $obj->rasterize($rast);
+        $obj->setStyle('visibility', 'collapse');
+        $obj->rasterize($rast);
+    }
+}
