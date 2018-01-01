459dad51deb090c717d21025b2d0cb9e7c7b056f
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-01-01 22:59:01 +0100

    Add tests for polygon, polyline, base class

---
+++ tests/Nodes/Shapes/SVGPolygonalShapeTest.php
@@ -0,0 +1,120 @@
+<?php
+
+use SVG\Nodes\Shapes\SVGPolygonalShape;
+
+/**
+ * @SuppressWarnings(PHPMD)
+ */
+class SVGPolygonalShapeSubclass extends SVGPolygonalShape
+{
+    const TAG_NAME = 'test_subclass';
+
+    public function __construct($points)
+    {
+        parent::__construct($points);
+    }
+
+    public function rasterize(\SVG\Rasterization\SVGRasterizer $rasterizer)
+    {
+    }
+}
+
+/**
+ * @SuppressWarnings(PHPMD)
+ */
+ class SVGPolygonalShapeTest extends PHPUnit_Framework_TestCase
+{
+    public function test__construct()
+    {
+        // should set provided points
+        $points = array(
+            array(42.5, 42.5),
+            array(37, 37),
+        );
+        $obj = new SVGPolygonalShapeSubclass($points);
+        $this->assertEquals($points, $obj->getPoints());
+    }
+
+    public function testConstructFromAttributes()
+    {
+        // should set empty points by default
+        $obj = SVGPolygonalShapeSubclass::constructFromAttributes(array());
+        $this->assertEquals(0, $obj->countPoints());
+
+        // should set points when provided
+        $obj = SVGPolygonalShapeSubclass::constructFromAttributes(array(
+            'points' => '1,1 2,2 3,3',
+        ));
+        $this->assertEquals(3, $obj->countPoints());
+    }
+
+    public function testAddPoint()
+    {
+        $obj = new SVGPolygonalShapeSubclass(array());
+
+        // should support 2 floats
+        $obj->addPoint(42.5, 42.5);
+        $this->assertEquals(array(
+            array(42.5, 42.5),
+        ), $obj->getPoints());
+
+        // should support an array
+        $obj->addPoint(array(37, 37));
+        $this->assertEquals(array(
+            array(42.5, 42.5),
+            array(37, 37),
+        ), $obj->getPoints());
+    }
+
+    public function testRemovePoint()
+    {
+        $obj = new SVGPolygonalShapeSubclass(array(
+            array(42.5, 42.5),
+            array(37, 37),
+        ));
+
+        // should remove points by index
+        $obj->removePoint(0);
+        $this->assertEquals(array(
+            array(37, 37),
+        ), $obj->getPoints());
+
+        $obj->removePoint(0);
+        $this->assertEquals(array(), $obj->getPoints());
+    }
+
+    public function testSetPoint()
+    {
+        $obj = new SVGPolygonalShapeSubclass(array(
+            array(42.5, 42.5),
+            array(37, 37),
+        ));
+
+        // should replace the point at the given index
+        $obj->setPoint(1, array(100, 100));
+        $this->assertEquals(array(
+            array(42.5, 42.5),
+            array(100, 100),
+        ), $obj->getPoints());
+    }
+
+    public function testGetSerializableAttributes()
+    {
+        $obj = new SVGPolygonalShapeSubclass(array(
+            array(42.5, 43.5),
+            array(37, 38),
+        ));
+        $obj->setAttribute('id', 'poly');
+
+        $attrs = $obj->getSerializableAttributes();
+
+        // should include other attributes
+        $this->assertArraySubset(array('id' => 'poly'), $attrs);
+
+        // should include 'points' attribute
+        $this->assertArrayHasKey('points', $attrs);
+
+        // should stringify correctly
+        $this->assertEquals('42.5,43.5 37,38', $attrs['points']);
+    }
+}
