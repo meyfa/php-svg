cf6c0171f2e6a27e817a360193c62ce484fbc6cb
Thomas Klein <thomas@bird.eu>
2018-02-02 17:58:32 +0100

    add text node types

---
+++ src/Nodes/Texts/SVGText.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SVG\Nodes\Texts;
+
+use SVG\Nodes\SVGNodeContainer;
+
+/**
+ * Represents the SVG tag 'text'.
+ */
+class SVGText extends SVGNodeContainer
+{
+    const TAG_NAME = 'text';
+}
