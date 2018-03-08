9c738174a1001b82bf60db0142d840c24f67fba3
Michael Große <mic.grosse@googlemail.com>
2018-03-08 13:48:16 +0100

    Add title type (#47)
    * feat: Add title type
    
    The value can be set with setValue as added in #42
    
    * feat(writer): prevent XSS in text nodes
    
    Textnodes are not intended to contain valid XML, this might be
    especially problematic if they contain javascript. Hence they need to be
    escaped in order to prevent cross site scripting attacks.
    
    * refactor(writer): clearer writeNode method
    
    This should make the writeNode method much easier to follow. It also
    prints the text from any SVGNode with value.
    
    * chore: suppress false positive phpmd error
    
    Since a title will not be rasterized at all, it makes sense that we do
    not use the provided parameter.
    
    * fix(writer): add missing return
    
    This prevents printing a second tag-closing '/>' which would lead to
    invalid xml.

---
+++ src/Nodes/Texts/SVGTitle.php
@@ -0,0 +1,24 @@
+<?php
+namespace SVG\Nodes\Texts;
+use SVG\Nodes\SVGNode;
+use SVG\Rasterization\SVGRasterizer;
+/**
+ * Represents the SVG tag 'title'.
+ * Has the attribute 'text'.
+ */
+class SVGTitle extends SVGNode
+{
+    const TAG_NAME = 'title';
+
+    /**
+     * Dummy implementation
+     *
+     * @param SVGRasterizer $rasterizer
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function rasterize(SVGRasterizer $rasterizer)
+    {
+        // nothing to rasterize
+    }
+}
