77a1a94ea5f2f23195f1ea48c7e9c68939e23bfa
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-03-11 12:15:20 +0100

    Add image assertions for SVGRectRender (w/o rx, ry)

---
+++ tests/Rasterization/Renderers/SVGRectRendererTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace SVG;
+
+use AssertGD\GDSimilarityConstraint;
+
+use SVG\Rasterization\SVGRasterizer;
+use SVG\Rasterization\Renderers\SVGRectRenderer;
+
+/**
+ * @SuppressWarnings(PHPMD)
+ */
+class SVGRectRendererTest extends \PHPUnit\Framework\TestCase
+{
+    public function testShouldRenderStroke()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', 'none');
+        $context->setStyle('stroke', '#FF0000');
+        $context->setStyle('stroke-width', '1px');
+
+        $rasterizer = new SVGRasterizer('40px', '40px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '4px', 'y' => '8px',
+            'width' => '20px', 'height' => '16px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-stroke.png'));
+    }
+
+    public function testShouldRenderStrokeThick()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', 'none');
+        $context->setStyle('stroke', '#FF0000');
+        $context->setStyle('stroke-width', '3px');
+
+        $rasterizer = new SVGRasterizer('20px', '20px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '2px', 'y' => '4px',
+            'width' => '10px', 'height' => '8px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-stroke-thick.png'));
+    }
+
+    public function testShouldRenderStrokeAlpha()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', 'none');
+        $context->setStyle('stroke', 'rgba(255, 0, 0, 0.5)');
+        $context->setStyle('stroke-width', '3px');
+
+        $rasterizer = new SVGRasterizer('20px', '20px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '2px', 'y' => '4px',
+            'width' => '10px', 'height' => '8px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-stroke-alpha.png'));
+    }
+
+    public function testShouldRenderFill()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', '#FF0000');
+        $context->setStyle('stroke', 'none');
+
+        $rasterizer = new SVGRasterizer('20px', '20px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '2px', 'y' => '4px',
+            'width' => '10px', 'height' => '8px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-fill.png'));
+    }
+
+    public function testShouldRenderFillAlpha()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', 'rgba(255, 0, 0, 0.5)');
+        $context->setStyle('stroke', 'none');
+
+        $rasterizer = new SVGRasterizer('20px', '20px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '2px', 'y' => '4px',
+            'width' => '10px', 'height' => '8px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-fill-alpha.png'));
+    }
+
+    public function testShouldRenderStrokeAndFill()
+    {
+        $obj = new SVGRectRenderer();
+
+        $context = $this->getMockForAbstractClass('\SVG\Nodes\SVGNode');
+        $context->setStyle('fill', 'rgba(255, 255, 255, 0.5)');
+        $context->setStyle('stroke', 'rgba(0, 0, 0, 0.5)');
+        $context->setStyle('stroke-width', '5px');
+
+        $rasterizer = new SVGRasterizer('40px', '40px', null, 40, 40);
+        $obj->render($rasterizer, array(
+            'x' => '4px', 'y' => '8px',
+            'width' => '20px', 'height' => '16px',
+        ), $context);
+        $img = $rasterizer->finish();
+
+        $this->assertThat($img,
+            new GDSimilarityConstraint('./tests/images/renderer-rect-stroke-fill.png'));
+    }
+}
