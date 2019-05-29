b188d5101d30991ca35e06bfdd7a54e3d4fbac59
Corleone87 <47756192+Corleone87@users.noreply.github.com>
2019-05-29 10:34:02 +0200

    Update SVGTextPath
    Now u can add an child to text and crate an textpath

--- src/Nodes/Texts/SVGTextPath.php
+++ src/Nodes/Texts/SVGTextPath.php
@@ -3,6 +3,7 @@
 namespace SVG\Nodes\Texts;
 
 use SVG\Nodes\SVGNodeContainer;
+use SVG\Rasterization\SVGRasterizer;
 
 /**
  * Represents the SVG tag 'textPath'.
@@ -10,4 +11,25 @@ use SVG\Nodes\SVGNodeContainer;
 class SVGTextPath extends SVGNodeContainer
 {
     const TAG_NAME = 'textPath';
+    
+    public function __construct($text = '', $href = '')
+	{
+		parent::__construct();
+		$this->setValue($text);
+
+		$this->setAttribute('xlink:href', $href);
+
+	}
+
+	public function rasterize(SVGRasterizer $rasterizer)
+	{
+		if (empty($this->font)) {
+			return;
+		}
+
+		$rasterizer->render('textPath', array(
+			'xlink:href' => $this->getAttribute('xlink:href'),
+			'text'       => $this->getValue()
+		), $this);
+	}
 }
