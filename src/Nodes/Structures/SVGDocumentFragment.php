6706be2b5c12d7242a4f168b9ca4d70481bbb97b
Nathanaël <roipoussiere@protonmail.com>
2021-06-23 22:34:35 +0200

    text raster: add style and weight filters

--- src/Nodes/Structures/SVGDocumentFragment.php
+++ src/Nodes/Structures/SVGDocumentFragment.php
@@ -83,21 +83,24 @@ class SVGDocumentFragment extends SVGNodeContainer
     }
 
     /**
-     * Add a font to text nodes.
+     * Add a font to text nodes that match given attributes.
      * @param string $fontPath The path of the font being added.
-     * @param string $nodeList The node list on where text nodes will be affected (whole document by default).
-     * @param string $fontFamily If specified, filter texts that are related to the given family.
+     * @param SVGText $textNode If specified, affects only text node that included in this node.
+     * @param string $fontFamily If specified, affects only texts that have this font family.
+     * @param string $fontWeight If specified, affects only texts that have this font weight.
+     * @param string $fontStyle If specified, affects only texts that have this text style.
      */
-    public function addFont(string $fontPath, ?DOMNodeList $nodeList = null, string $fontFamily = '') : void
+    public function addFont(string $fontPath, ?\SVG\Nodes\Texts\SVGText $textNode = null, string $fontFamily = '',
+                            string $fontWeight = '', string $fontStyle = '') : void
     {
-        $nodeList = $nodeList == null ? $this : $nodeList;
-        $this->fonts[$fontFamily] = $fontPath;
-
-        foreach ($nodeList->getElementsByTagName('text') as $textElmt) {
-            $font_family = $textElmt->getComputedStyle('font-family') ?? '';
-            if ($fontFamily == '' || $fontFamily == $font_family) {
-                $textElmt->setValue(preg_replace('/[\x00-\x1F\x7F]/u', '', $textElmt->getValue()));
-                $textElmt->setFont(new \SVG\Nodes\Structures\SVGFont($fontFamily, $fontPath));
+        $textNodes = $textNode == null ? $this->getElementsByTagName('text') : [$textNode];
+        foreach ($textNodes as $textNode) {
+            $nodeFontFamily = $textNode->getComputedStyle('font-family') ?? '';
+            $nodeFontWeight = $textNode->getComputedStyle('font-weight') ?? '';
+            $nodeFontStyle = $textNode->getComputedStyle('font-style') ?? '';
+            if ($nodeFontFamily == $fontFamily && $nodeFontWeight == $fontWeight && $nodeFontStyle == $fontStyle) {
+                $textNode->setValue(preg_replace('/[\x00-\x1F\x7F]/u', '', $textNode->getValue()));
+                $textNode->setFont(new \SVG\Nodes\Structures\SVGFont($fontFamily, $fontPath));
             }
         }
     }
