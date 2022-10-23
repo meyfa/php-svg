85d8c87a7e9f4bfe33391ab8186717a693d27e5b
Niellles <10075507+Niellles@users.noreply.github.com>
2022-10-23 21:38:15 +0200

    chore: Improve each() polyfill
    Previously tests/bootstrap.php had to be loaded before initializing the PHPUnit test framework. Instead of doing this via loading bootstrap.php (configured in pipeline and specified manually via CLI) the each() polyfill is now automatically loaded via the composer autoloader (dev only).
    
    fix: Wrong composer.json
    
    Moved some stuff around after the results of this chore were tested locally.
    Entry for autoloading functions.php ended up under psr-4 which results in an error. This has now been fixed.
    
    chore: fix codestyle
    
    Fixed some code style issues.

--- src/Shims/functions.php
+++ src/Shims/functions.php
@@ -2,12 +2,22 @@
 
 /*
  * This file provides functions from older PHP versions that are no longer present in newer versions.
- * This is needed because we are on PHPUnit 4.8 which was built long before some things were deprecated.
  *
- * Load this file before anything else is run, preferably through php.ini auto_prepend_file.
  */
 
 if (!function_exists('each')) {
+    /**
+     * Return the current key and value pair from an array and advance the array cursor.
+     * After each() has executed, the array cursor will be left on the next element of the array,
+     * or past the last element if it hits the end of the array.
+     * You have to use reset() if you want to traverse the array again using each.
+     *
+     * This is needed because we are on PHPUnit 4.8 which was built long before each was deprecated.
+     *
+     * @param $array
+     *
+     * @return array|false|null
+     */
     function each(&$array)
     {
         if (!is_array($array) && !is_object($array)) {
