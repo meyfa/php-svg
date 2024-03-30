cd666e3060ae197ce37f4a6e620ae6f0a5254690
Simon André <smn.andre@gmail.com>
2024-03-31 00:49:30 +0100

    ci: Migrate to PHP-CS-Fixer (#230)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+    ])
+    ->setFinder($finder)
+;
