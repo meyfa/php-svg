6b04a3b1c1dd56b29cfd5b9fc0f0716dd14c1afc
Simon André <smn.andre@gmail.com>
2024-03-30 15:29:44 +0100

    Add "no yoda condition" rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,11 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
     ])
     ->setFinder($finder)
 ;
