3712fa0036d490984c01d0ee16c7f032e9bfde17
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-03-31 13:47:38 +0200

    chore: Enable more PHP-CS-Fixer rules
    Continuation of #230. Closes #229.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,8 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
+        'backtick_to_shell_exec' => true,
+        'no_alias_functions' => true,
         'yoda_style' => [
             'equal' => false,
             'identical' => false,
